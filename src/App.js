4b146efdc026e8596fc7c60c37b8a778f66a1d39
Jonathan Cho <joncho26@gmail.com>
2018-05-09 11:08:20 -0400

    add boilerplate for app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Chat from './components/Chat';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Chat />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Chat.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Chat extends Component {
+  constructor(){
+    super();
+  }
+  render(){
+    return (
+      <div>
+        Chat
+      </div>
+    )
+  }
+
+}
+export default Chat;
